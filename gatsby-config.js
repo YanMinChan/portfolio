5352ac516bd358aa13e165465e8e2a8c463ab50d
YanMinChan <yanmin.chan08@gmail.com>
2025-10-02 23:42:54 +0800

    add deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,8 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Yan Min Chan',
-    description:
-      '',
-    siteUrl: 'https://yanminchan.com', // No trailing slash allowed!
+    description: '',
+    siteUrl: 'https://yanminchan.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
